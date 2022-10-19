b346b492c6a901ef4c4bfb7a67d722778cd4d6de
tattersol <106263649+Bjcostello@users.noreply.github.com>
2022-10-19 15:49:20 +0000

    HW9P4 completion

--- .ccls-cache/@home@runner@CSCI130Homework9problems/HW9P4@HW9P4.cpp
+++ .ccls-cache/@home@runner@CSCI130Homework9problems/HW9P4@HW9P4.cpp
@@ -9,6 +9,9 @@ using namespace std;
 void printMatrix_3col(int matrix[][3], int N_ROWS, int N_COLUMNS);
 int CountUnhealthySensitive(int matrix[][3], int N_ROWS, int N_COLUMNS,
                             int minVal, int maxVal);
+int CountUnhealthyCity(int matrix[][3], int N_ROWS, int city_col);
+
+int MeanAirQualityIndex(int matrix[][3], int N_ROWS, int city_col);
 
 int main() {
 
@@ -18,10 +21,28 @@ int main() {
                                  {17, 13, 27}};
 
   printMatrix_3col(AirQuality_Index, 10, 3);
+  cout << endl;
 
-  cout
-      << "Number of days with air quality \'Unhelalthy for sensitive groups\': "
-      << CountUnhealthySensitive(AirQuality_Index, 10, 3, 101, 150) << endl;
+  cout << "From 10/4/2020 to 10/13/2020 the air quality was unhealthy for "
+          "sensitive groups in: "
+       << endl;
+
+  cout << "Grand Junction: " << CountUnhealthyCity(AirQuality_Index, 10, 0)
+       << " times" << endl;
+  cout << "Fort Collins: " << CountUnhealthyCity(AirQuality_Index, 10, 1)
+       << " times" << endl;
+  cout << "Denver - Boulder: " << CountUnhealthyCity(AirQuality_Index, 10, 2)
+       << " times" << endl
+       << endl;
+
+  cout << "From 10/4/2020 to 10/13/2020 the air quality mean was: " << endl;
+
+  cout << "Grand Junction: " << MeanAirQualityIndex(AirQuality_Index, 10, 0)
+       << endl;
+  cout << "Fort Collins: " << MeanAirQualityIndex(AirQuality_Index, 10, 1)
+       << endl;
+  cout << "Denver - Boulder: " << MeanAirQualityIndex(AirQuality_Index, 10, 2)
+       << endl;
   return 0;
 }
 
@@ -49,4 +70,27 @@ int CountUnhealthySensitive(int matrix[][3], int N_ROWS, int N_COLUMNS,
     }
   }
   return count;
+}
+
+// function to do search through a column and return a count of values within a
+// specific range
+int CountUnhealthyCity(int matrix[][3], int N_ROWS, int city_col) {
+  int row;
+  int count = 0;
+  for (row = 0; row < N_ROWS; row++) {
+    if (matrix[row][city_col] >= 101) {
+      count++;
+    }
+  }
+  return count;
+}
+
+int MeanAirQualityIndex(int matrix[][3], int N_ROWS, int city_col) {
+  int sum = 0;
+  int row, avg;
+
+  for (row = 0; row < N_ROWS; row++)
+    sum = sum + matrix[row][city_col];
+  avg = sum / N_ROWS;
+  return avg;
 }
